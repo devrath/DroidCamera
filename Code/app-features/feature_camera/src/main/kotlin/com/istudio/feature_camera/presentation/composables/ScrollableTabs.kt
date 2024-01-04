62ee4bbce3a675377903160558dfdf7f762f693a
Devrath <devrath.dev595@gmail.com>
2024-01-04 10:23:19 +0530

    added

--- Code/app-features/feature_camera/src/main/kotlin/com/istudio/feature_camera/presentation/composables/ScrollableTabs.kt
+++ Code/app-features/feature_camera/src/main/kotlin/com/istudio/feature_camera/presentation/composables/ScrollableTabs.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
@@ -34,12 +38,16 @@ fun ScrollableTabs(
 ) {
 
     val itemHorizontalPadding = 26.dp
+    val backgroundShapeWidth = 62.dp
     val screenHalfWidth = LocalConfiguration.current.screenWidthDp.div(2).dp
     val horizontalContentPadding = screenHalfWidth.minus(itemHorizontalPadding.div(2))
 
     val coroutineScope = rememberCoroutineScope()
     val lazyListState = rememberLazyListState()
     val flingBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState)
+
+    val items = Tabs.values()
+
 
     Column(
         modifier = Modifier
@@ -49,28 +57,30 @@ fun ScrollableTabs(
         verticalArrangement = Arrangement.Center
     ) {
 
-        val items = Tabs.values()
+        Box(contentAlignment = Alignment.Center) {
 
-        LazyRow(
-            state = lazyListState,
-            flingBehavior = flingBehavior,
-            contentPadding = PaddingValues(horizontal = horizontalContentPadding),
-            horizontalArrangement = Arrangement.spacedBy(26.dp)
-        ){
-            items(items.size) { index ->
-                Text(
-                    text = items[index].name,
-                    style = MaterialTheme.typography.labelLarge,
-                    fontSize = 16.sp,
-                    color = Color.White,
-                    modifier = Modifier.padding(vertical = 20.dp).clickable {
-                        coroutineScope.launch {
-                            lazyListState.animateScrollToItem(index)
+            LazyRow(
+                state = lazyListState,
+                flingBehavior = flingBehavior,
+                contentPadding = PaddingValues(horizontal = horizontalContentPadding),
+                horizontalArrangement = Arrangement.spacedBy(26.dp)
+            ){
+                items(items.size) { index ->
+                    Text(
+                        text = items[index].name,
+                        style = MaterialTheme.typography.labelLarge,
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 20.dp).clickable {
+                            coroutineScope.launch {
+                                lazyListState.animateScrollToItem(index)
+                            }
                         }
-                    }
-                )
+                    )
+                }
             }
         }
+
     }
 
 }
